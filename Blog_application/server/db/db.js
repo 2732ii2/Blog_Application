3ac589592e130ef907034591bfd7e9ddbf146b73
Ahmad <Ahmad>
2023-04-27 11:13:49 +0530

    Deploy_All

---
+++ Blog_application/server/db/db.js
@@ -0,0 +1,52 @@
+
+
+import mongoose from "mongoose";
+
+
+
+export async function connection()
+{
+    try{
+        var connect=await mongoose.connect("mongodb://localhost:27017/blog_api", {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            });
+        console.log("connected db... ");
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// var pass="Ashad123";
+// const URL= `mongodb+srv://Ashad123:${pass}@cluster0.k614zrs.mongodb.net/?retryWrites=true&w=majority`;
+
+// export async function connection ()
+// {
+//     try
+//     {
+//         await mongoose.connect(URL, {
+//         useUnifiedTopology: true,
+//         useNewUrlParser: true,
+//         });
+//         console.log("db as connected");
+//     }
+//     catch(err)
+//     {
+//         console.log("the error is comming", err);
+//     }
+
+// }
+
+
+
